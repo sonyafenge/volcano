ecba659e0c3663c46340fc312aed5053c1061341
Sonya Li <sonya.li@omniva.com>
2024-12-05 15:56:16 -0800

    kwok with vcjob testing

--- benchmark/sh/query.sh
+++ benchmark/sh/query.sh
@@ -21,6 +21,8 @@ start_ts_default=$2  # 第一个时间段的开始时间
 end_ts_default=$3    # 第一个时间段的结束时间
 start_ts_volcano=$4  # 第二个时间段的开始时间
 end_ts_volcano=$5    # 第二个时间段的结束时间
+start_ts_vcjob=$8  # 第三个时间段的开始时间
+end_ts_vcjob=$9    # 第三个时间段的结束时间
 aff=$6
 use_mini=$7 # 是否使用mini volcano
 
@@ -31,8 +33,8 @@ if [ -z "$group" ]; then
 fi
 
 # 校验时间参数个数，必须为7个
-if [ $# -ne 7 ]; then
-  echo "must have group,start_ts_default,end_ts_default,start_ts_volcano,end_ts_volcano,aff,use_mini"
+if [ $# -ne 9 ]; then
+  echo "must have group,start_ts_default,end_ts_default,start_ts_volcano,end_ts_volcano,aff,use_mini,start_ts_vcjob,end_ts_vcjob"
   exit 1
 fi
 
@@ -58,12 +60,14 @@ sleep 3
 # 配置 Prometheus 的 URL 和查询
 PROMETHEUS_URL="http://localhost:9090/api/v1/query_range"
 QUERY="count(kube_pod_container_status_ready\{namespace=%22default%22\})"
+QUERY_VCJOB="count(kube_pod_status_phase\{phase=%22Succeeded%22,namespace=%22default%22\})"
 
 # 时间步长（每60秒采样一次）
 STEP=1s
 
 echo "$PROMETHEUS_URL?query=$QUERY&start=$start_ts_default&end=$end_ts_default&step=$STEP"
 echo "$PROMETHEUS_URL?query=$QUERY&start=$start_ts_volcano&end=$end_ts_volcano&step=$STEP"
+echo "$PROMETHEUS_URL?query=$QUERY_VCJOB&start=$start_ts_vcjob&end=$end_ts_vcjob&step=$STEP"
 
 # 获取第一个时间段的数据
 DATA1=$(curl -s "$PROMETHEUS_URL?query=$QUERY&start=$start_ts_default&end=$end_ts_default&step=$STEP" | jq '.data.result[0].values')
@@ -71,24 +75,30 @@ DATA1=$(curl -s "$PROMETHEUS_URL?query=$QUERY&start=$start_ts_default&end=$end_t
 # 获取第二个时间段的数据
 DATA2=$(curl -s "$PROMETHEUS_URL?query=$QUERY&start=$start_ts_volcano&end=$end_ts_volcano&step=$STEP" | jq '.data.result[0].values')
 
+# 获取第三个时间段的数据
+DATA3=$(curl -s "$PROMETHEUS_URL?query=$QUERY_VCJOB&start=$start_ts_vcjob&end=$end_ts_vcjob&step=$STEP" | jq '.data.result[0].values')
+
 # 提取时间和count值，将两个时间段的起点对齐
 start_ts_default_ALIGNED=$start_ts_default
 start_ts_volcano_ALIGNED=$start_ts_default  # 将第二个时间段的起点对齐到第一个时间段
+start_ts_vcjob_ALIGNED=$start_ts_default  # 将第三个时间段的起点对齐到第一个时间段
 
 # 处理数据，将数据保存到文件
-echo "# Time (aligned)  Count1  Count2" > data.txt
+echo "# Time (aligned)  Count1  Count2 Count3" > data.txt
 for (( i=0; i<$(echo $DATA1 | jq 'length'); i++ )); do
   TIME1=$(echo $DATA1 | jq -r ".[$i][0]")
   COUNT1=$(echo $DATA1 | jq -r ".[$i][1]")
   TIME2=$(echo $DATA2 | jq -r ".[$i][0]")
   COUNT2=$(echo $DATA2 | jq -r ".[$i][1]")
+  TIME3=$(echo $DATA3 | jq -r ".[$i][0]")
+  COUNT3=$(echo $DATA3 | jq -r ".[$i][1]")
 
   # 对齐时间到 start_ts_default
   ALIGNED_TIME1=$((TIME1 - start_ts_default_ALIGNED))
   ALIGNED_TIME2=$((TIME2 - start_ts_volcano_ALIGNED))
-
+  ALIGNED_TIME3=$((TIME3 - start_ts_vcjob_ALIGNED))
   # 将对齐后的时间和 count 值写入文件
-  echo "$ALIGNED_TIME1  $COUNT1  $COUNT2" >> data.txt
+  echo "$ALIGNED_TIME1  $COUNT1  $COUNT2 $COUNT3" >> data.txt
 done
 
 if [ "$aff" = true ]; then
@@ -105,7 +115,7 @@ fi
 # 使用 gnuplot 绘制对比图
 gnuplot <<EOF
 set terminal pngcairo size 800,600
-set output '../img/res/$img_name.png'
+set output '../img/kamaji/$img_name.png'
 set title "Group $group"
 set xlabel "Time (seconds from start)"
 set ylabel "Ready Pod Count"
@@ -113,7 +123,8 @@ set grid
 
 set yrange [0:5100]  # 自动扩展 Y 轴，但从 0 开始
 plot "data.txt" using 1:2 with lines title "Default", \
-     "data.txt" using 1:3 with lines title "Volcano"
+     "data.txt" using 1:3 with lines title "Volcano", \
+     "data.txt" using 1:4 with lines title "Vcjob"
 EOF
 
 echo "benchmark对比图已生成：$img_name.png"
